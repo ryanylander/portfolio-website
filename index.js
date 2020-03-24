6dbd61e2989d04141cd6e0cf4aa06a2daad0b5fe
Ryan Scott Nylander <nylander@udel.edu>
2020-03-23 22:42:48 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+//Look for an env variable called PORT, or default to 5000
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
